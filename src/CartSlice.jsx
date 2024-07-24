c971f85b94accdd6066f307633b49098d905279a
MyriamAA <myriamabouatmeh@gmail.com>
2024-07-24 11:52:34 +0300

    Update cart reducer

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const plant = action.payload;
+      const { name, cost } = plant;
+      const existingPlant = state.items.find((p) => p.name === name);
+      if (existingPlant) {
+        existingPlant.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const name = action.payload;
+      const existingPlant = state.items.find((p) => p.name === name);
+
+      if (existingPlant) {
+        state.items = state.items.filter((item) => item.name !== name);
+      }
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
 
-    
+      const plant = state.items.find((plant) => plant.name === name);
+
+      if (plant) {
+        plant.quantity = quantity;
+      }
     },
   },
 });
